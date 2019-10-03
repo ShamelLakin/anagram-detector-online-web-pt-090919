347b8a802983d04538c66ba49b924ac520321159
Shamel Lakin <christlife4u@gmail.com>
2019-10-03 20:34:06 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,6 +8,7 @@ class Anagram
   def match(word_array) 
     word_array.find_all do |w|
       (@word.split("").sort) == (w.split("").sort)
+      # split a word into an array of letters using some_word.split(""). You can compare two arrays using the ==
     end
   end
   
