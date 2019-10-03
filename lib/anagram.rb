062f7327bb91c72001243ba355c3f8b88f78edc2
Shamel Lakin <christlife4u@gmail.com>
2019-10-03 20:34:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,6 +8,7 @@ class Anagram
   def match(word_array) 
     word_array.find_all do |w|
       (@word.split("").sort) == (w.split("").sort)
+      
       # split a word into an array of letters using @word.split("") & comparing two arrays using the == .
     end
   end
